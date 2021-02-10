d9c2bae7ee64b3236bebb2167e5576131a86c2b9
Martijn Gribnau <garm@ilumeo.com>
2021-02-10 19:07:15 +0100

    Add test cases for stable, beta and nightly releases

--- src/lib.rs
+++ src/lib.rs
@@ -21,21 +21,21 @@ mod release_manifest;
 #[cfg(test)]
 mod tests {
     use super::*;
+    use yare::parameterized;
 
-    #[test]
-    fn pass() {
-        let path = [
-            env!("CARGO_MANIFEST_DIR"),
-            "/resources/stable_2016-04-12.toml",
-        ]
-        .join("");
+    #[parameterized(
+        stable = { "/resources/stable_2016-04-12.toml", "1.8.0" },
+        beta = { "/resources/beta_2016-03-23.toml", "1.8.0-beta.2" },
+        nightly = { "/resources/nightly_2016-03-08.toml", "1.9.0-nightly" },
+    )]
+    fn pass(resource: &str, expected_version: &str) {
+        let expected_version = semver::Version::parse(expected_version).unwrap();
+
+        let path = [env!("CARGO_MANIFEST_DIR"), resource].join("");
         let index = ReleaseIndex::try_from_documents(&vec![DocumentSource::LocalPath(path.into())])
             .unwrap();
 
         assert!(index.releases().len() > 0);
-        assert_eq!(
-            index.releases()[0].version(),
-            &semver::Version::new(1, 8, 0)
-        );
+        assert_eq!(index.releases()[0].version(), &expected_version);
     }
 }

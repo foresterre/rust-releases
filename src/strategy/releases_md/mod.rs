69e4c827ff189abd17e0809664190a37ed66b1e0
Martijn Gribnau <garm@ilumeo.com>
2021-02-23 07:15:47 +0100

    Implement Strategy trait releases_md strategy

--- src/strategy/releases_md/mod.rs
+++ src/strategy/releases_md/mod.rs
@@ -1,6 +1,6 @@
 use crate::source::DocumentSource;
 use crate::strategy::{FetchResources, Strategy};
-use crate::{Channel, ReleaseIndex, TResult};
+use crate::{Channel, Release, ReleaseIndex, TResult};
 
 pub struct ReleasesMd {
     source: DocumentSource,
@@ -15,12 +15,52 @@ impl ReleasesMd {
 
 impl Strategy for ReleasesMd {
     fn build_index(&self) -> TResult<ReleaseIndex> {
-        unimplemented!()
+        let contents = self.source.load()?;
+        let content = String::from_utf8(contents).map_err(ReleasesMdError::UnrecognizedText)?;
+
+        let releases = content
+            .lines()
+            .filter(|s| s.starts_with("Version"))
+            .filter_map(|s| {
+                s.split_ascii_whitespace().skip(1).next().and_then(|s| {
+                    semver::Version::parse(s)
+                        .map(|version| Release::new(version))
+                        .ok()
+                })
+            });
+
+        Ok(ReleaseIndex::new(releases))
     }
 }
 
 impl FetchResources for ReleasesMd {
     fn fetch_channel(_channel: Channel) -> TResult<Self> {
         unimplemented!()
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ReleasesMdError {
+    #[error("{0}")]
+    UnrecognizedText(#[from] std::string::FromUtf8Error),
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::source::DocumentSource;
+    use crate::strategy::releases_md::ReleasesMd;
+    use crate::ReleaseIndex;
+
+    #[test]
+    fn strategy_dist_index() {
+        let path = [
+            env!("CARGO_MANIFEST_DIR"),
+            "/resources/releases_md/RELEASES.md",
+        ]
+        .join("");
+        let strategy = ReleasesMd::from_document(DocumentSource::LocalPath(path.into()));
+        let index = ReleaseIndex::with_strategy(strategy).unwrap();
+
+        assert!(index.releases().len() > 50);
     }
 }

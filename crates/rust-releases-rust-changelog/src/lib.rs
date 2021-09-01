6c4610b666fdda84946b49e0e340b569314f9308
Martijn Gribnau <garm@ilumeo.com>
2021-09-01 17:44:05 +0200

    Support unreleased versions in the changelog

--- crates/rust-releases-rust-changelog/src/lib.rs
+++ crates/rust-releases-rust-changelog/src/lib.rs
@@ -4,7 +4,6 @@
 //! Please, see the [`rust-releases`] for additional documentation on how this crate can be used.
 //!
 //! [`rust-releases`]: https://docs.rs/rust-releases
-
 use rust_releases_core::{semver, Channel, FetchResources, Release, ReleaseIndex, Source};
 use rust_releases_io::Document;
 #[cfg(test)]
@@ -17,18 +16,34 @@ pub(crate) mod fetch;
 use crate::fetch::fetch;
 
 pub use errors::{RustChangelogError, RustChangelogResult};
+use std::str::FromStr;
 
 /// A [`Source`] which obtains release data from the official Rust changelog.
 ///
 /// [`Source`]: rust_releases_core::Source
 pub struct RustChangelog {
     source: Document,
+
+    /// Used to compare against the date of an unreleased version which does already exist in the
+    /// changelog. If this date is at least as late as the time found in a release registration, we
+    /// will say that such a version is released (i.e. published).
+    today: ReleaseDate,
 }
 
 impl RustChangelog {
-    #[cfg(test)]
     pub(crate) fn from_document(source: Document) -> Self {
-        Self { source }
+        Self {
+            source,
+            today: ReleaseDate::today(),
+        }
+    }
+
+    #[cfg(test)]
+    pub(crate) fn from_document_with_date(source: Document, date: ReleaseDate) -> Self {
+        Self {
+            source,
+            today: date,
+        }
     }
 }
 
@@ -42,13 +57,31 @@ impl Source for RustChangelog {
         let releases = content
             .lines()
             .filter(|s| s.starts_with("Version"))
-            .filter_map(|s| {
-                s.split_ascii_whitespace()
-                    .nth(1)
-                    .and_then(|s| semver::Version::parse(s).map(Release::new_stable).ok())
-            });
+            .filter_map(|line| {
+                let parsed = parse_release(line.split_ascii_whitespace());
 
-        Ok(releases.collect())
+                match parsed {
+                    // If the version and date can be parsed, and the version has been released
+                    Ok((version, date)) if date.is_available(&self.today) => {
+                        Some(Ok(Release::new_stable(version)))
+                    }
+                    // If the version and date can be parsed, but the version is not yet released
+                    Ok(_) => None,
+                    // We skip versions 0.10, 0.9, etc. which require more lenient semver parsing
+                    // Unfortunately we can't access the error kind, so we have to match the string instead
+                    Err(RustChangelogError::SemverError(err, _))
+                        if err.to_string().as_str()
+                            == "unexpected end of input while parsing minor version number" =>
+                    {
+                        None
+                    }
+                    // In any ony other error case, we forward the error
+                    Err(err) => Some(Err(err)),
+                }
+            })
+            .collect::<Result<ReleaseIndex, Self::Error>>()?;
+
+        Ok(releases)
     }
 }
 
@@ -57,16 +90,63 @@ impl FetchResources for RustChangelog {
 
     fn fetch_channel(channel: Channel) -> Result<Self, Self::Error> {
         if let Channel::Stable = channel {
-            let source = fetch()?;
-            Ok(Self { source })
+            let document = fetch()?;
+            Ok(Self::from_document(document))
         } else {
             Err(RustChangelogError::ChannelNotAvailable(channel))
         }
     }
 }
 
+fn parse_release<'line>(
+    mut parts: impl Iterator<Item = &'line str>,
+) -> Result<(semver::Version, ReleaseDate), RustChangelogError> {
+    let version_number = parts
+        .nth(1)
+        .ok_or(RustChangelogError::NoVersionInChangelogItem)?;
+    let release_date = parts
+        .next()
+        .ok_or(RustChangelogError::NoDateInChangelogItem)?;
+
+    let version = semver::Version::parse(version_number)
+        .map_err(|err| RustChangelogError::SemverError(err, version_number.to_string()))?;
+
+    let date = ReleaseDate::parse(&release_date[1..release_date.len() - 1])?;
+
+    Ok((version, date))
+}
+
+#[derive(Debug)]
+struct ReleaseDate(chrono::NaiveDate);
+
+impl ReleaseDate {
+    fn today() -> Self {
+        Self(chrono::Utc::today().naive_utc())
+    }
+
+    fn parse(from: &str) -> Result<Self, RustChangelogError> {
+        from.parse::<ReleaseDate>()
+    }
+
+    fn is_available(&self, today: &Self) -> bool {
+        today.0 >= self.0
+    }
+}
+
+impl FromStr for ReleaseDate {
+    type Err = crate::RustChangelogError;
+
+    fn from_str(item: &str) -> Result<Self, Self::Err> {
+        let result = chrono::NaiveDate::parse_from_str(item, "%Y-%m-%d")
+            .map_err(|_| RustChangelogError::ChronoParseError(item.to_string()))?;
+
+        Ok(Self(result))
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::ReleaseDate;
     use crate::RustChangelog;
     use rust_releases_core::{semver, Channel, FetchResources, Release, ReleaseIndex};
     use rust_releases_io::Document;
@@ -86,6 +166,33 @@ mod tests {
         assert_eq!(
             index.releases()[0],
             Release::new_stable(semver::Version::new(1, 50, 0))
+        );
+    }
+
+    #[test]
+    fn parse_date() {
+        let date = ReleaseDate::parse("2021-09-01").unwrap();
+        assert_eq!(date.0, chrono::NaiveDate::from_ymd(2021, 09, 01));
+    }
+
+    #[test]
+    fn with_unreleased_version() {
+        let path = [
+            env!("CARGO_MANIFEST_DIR"),
+            "/../../resources/rust_changelog/RELEASES_with_unreleased.md",
+        ]
+        .join("");
+
+        let date = ReleaseDate::parse("2021-09-01").unwrap();
+        let strategy =
+            RustChangelog::from_document_with_date(Document::LocalPath(path.into()), date);
+        let index = ReleaseIndex::from_source(strategy).unwrap();
+
+        let mut releases = index.releases().iter();
+
+        assert_eq!(
+            releases.next().unwrap().version(),
+            &semver::Version::new(1, 54, 0)
         );
     }
 

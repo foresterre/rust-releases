7cb5552a8ef2a5487f3e4c3ee1b3eeaf3398e7a1
Martijn Gribnau <garm@ilumeo.com>
2021-10-15 17:02:44 +0200

    Add short project description to rust-toolchain project docs

--- crates/rust-toolchain/src/lib.rs
+++ crates/rust-toolchain/src/lib.rs
@@ -1,1 +1,21 @@
-
+//! # rust-toolchain
+//!
+//! The `rust-toolchain` crate defines a set of types which describe a Rust toolchain.
+//! While there is no definitive spec which defines what a "Rust toolchain" is,
+//! we try to follow the official Rust release process as closely as possible.
+//! The [`rustup`] project has written down a rough specification for [`toolchains`] used
+//! by the Rust project. In the initial version, we will follow this spec, but disregard custom
+//! toolchains altogether, in the name of simplicity.
+//!
+//! This project is part of the [`rust-releases`] and [`cargo-msrv`] projects.
+//! In case you have a feature request, question, bug, or have another reason to contact the developers,
+//! please, create a new issue at the `rust-releases` [`repository`].
+//!
+//! [`rustup`]: https://github.com/rust-lang/rustup
+//! [`toolchains`]: https://rust-lang.github.io/rustup/concepts/toolchains.html
+//! [`rust-releases`]: https://github.com/foresterre/rust-releases
+//! [`cargo-msrv`]: https://github.com/foresterre/cargo-msrv
+//! [`repository`]: https://github.com/foresterre/rust-releases/issues
+#![deny(missing_docs)]
+#![deny(clippy::all)]
+#![deny(unsafe_code)]

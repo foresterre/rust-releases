6e350a2b69e4d73c9a2b4364c6f7edfb3e3b3cf0
Martijn Gribnau <garm@ilumeo.com>
2023-03-19 08:38:18 +0100

    Update example

--- examples/list-stable.rs
+++ examples/list-stable.rs
@@ -1,21 +1,90 @@
 use rust_releases::FetchResources;
 use rust_releases_core::{Channel, ReleaseIndex};
 use rust_releases_rust_changelog::RustChangelog;
-use std::io::{stdout, Write};
+#[cfg(feature = "rust-dist")]
+use rust_releases_rust_dist::RustDist;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let source = RustChangelog::fetch_channel(Channel::Stable)?;
-    let index = ReleaseIndex::from_source(source)?;
+    let source = std::env::args()
+        .nth(1)
+        .unwrap_or("rust-changelog".to_string());
+    let source_type = SourceType::from(source.as_str());
 
-    let stdout = stdout();
-    let mut output = stdout.lock();
+    let client = Client::with_source(source_type);
+    let index = client.build_index();
 
-    let _ = output.write("source: rust-changelog\n".as_bytes())?;
-    let _ = output.write(format!("parsed-releases: {}\n", index.releases().len()).as_bytes())?;
+    let releases = index.releases();
+    let releases_count = releases.len();
 
-    for release in index.releases() {
-        let _ = output.write(format!("{}\n", release.version()).as_bytes())?;
-    }
+    println!("source: {:?}", source_type);
+    println!("releases count: {}", releases_count);
+
+    let versions = releases
+        .iter()
+        .map(|release| format!("{}", release.version()))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    println!("release versions:\n{}", versions);
 
     Ok(())
+}
+
+#[derive(Clone, Copy, Debug)]
+enum SourceType {
+    RustChangelog,
+    #[cfg(feature = "rust-dist")]
+    RustDist,
+}
+
+impl<'a> From<&'a str> for SourceType {
+    fn from(value: &'a str) -> Self {
+        match value {
+            "rust-changelog" | "rustchangelog" | "changelog" => SourceType::RustChangelog,
+            "rust-dist" | "rustdist" | "dist" => {
+                #[cfg(feature = "rust-dist")]
+                {
+                    SourceType::RustDist
+                }
+
+                #[cfg(not(feature = "rust-dist"))]
+                {
+                    panic!(
+                        "Source '{}' is not supported, to enable, compile with `--features rust-dist`", value
+                    )
+                }
+            }
+            elsy => panic!("Source '{}' is not supported", elsy),
+        }
+    }
+}
+
+struct Client {
+    source_type: SourceType,
+}
+
+impl Client {
+    pub fn with_source(source_type: SourceType) -> Self {
+        Self { source_type }
+    }
+
+    pub fn build_index(&self) -> ReleaseIndex {
+        let channel = Channel::Stable;
+
+        match self.source_type {
+            SourceType::RustChangelog => {
+                let intermediate = RustChangelog::fetch_channel(channel)
+                    .expect("Unable to fetch 'rust-changelog'");
+                ReleaseIndex::from_source(intermediate)
+                    .expect("Unable to build index for 'rust-changelog'")
+            }
+            #[cfg(feature = "rust-dist")]
+            SourceType::RustDist => {
+                let intermediate =
+                    RustDist::fetch_channel(channel).expect("Unable to fetch 'rust-dist'");
+                ReleaseIndex::from_source(intermediate)
+                    .expect("Unable to build index for 'rust-dist'")
+            }
+        }
+    }
 }

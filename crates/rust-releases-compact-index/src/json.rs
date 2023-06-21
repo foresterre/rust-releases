c5c3b7405bce80b42a162847c1ba4245e0e31b55
Martijn Gribnau <garm@ilumeo.com>
2023-06-21 04:54:36 +0200

    Ignore try_from ChannelManifest argument for now

--- crates/rust-releases-compact-index/src/json.rs
+++ crates/rust-releases-compact-index/src/json.rs
@@ -5,7 +5,7 @@ pub struct JsonManifest;
 impl TryFrom<ChannelManifest> for JsonManifest {
     type Error = ();
 
-    fn try_from(value: ChannelManifest) -> Result<Self, Self::Error> {
+    fn try_from(_value: ChannelManifest) -> Result<Self, Self::Error> {
         todo!()
     }
 }

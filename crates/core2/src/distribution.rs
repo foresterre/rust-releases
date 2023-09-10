0df8a36f6ae461d125b7aa6cd6e53c705b85b72a
Martijn Gribnau <garm@ilumeo.com>
2023-09-10 04:04:35 +0200

    docs: add examples to the Distribution docs

--- crates/core2/src/distribution.rs
+++ crates/core2/src/distribution.rs
@@ -1,3 +1,6 @@
+//! See [`Distribution`].
+#![forbid(missing_docs)]
+
 #[cfg(test)]
 mod tests;
 
@@ -16,6 +19,21 @@ pub struct Distribution {
 
 impl Distribution {
     /// Create a new [`Distribution`] for a given toolchain with the given components.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// let date = ReleaseDate::new(2018, 12, 6);
+    /// let toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    /// let component = Component::new_component("some");
+    ///
+    /// // Creating a Rust distribution for Rust 1.31 which was released on 2018-12-06
+    /// let distribution = Distribution::new(date, toolchain, iter::once(component));
+    /// ```
     pub fn new(
         date: rust_toolchain::ReleaseDate,
         toolchain: rust_toolchain::Toolchain,
@@ -29,6 +47,20 @@ impl Distribution {
     }
 
     /// Create a new [`Distribution`] for a given toolchain, without any components.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// let date = ReleaseDate::new(2018, 12, 6);
+    /// let toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    ///
+    /// // Creating a Rust distribution for Rust 1.31 which was released on 2018-12-06
+    /// let distribution = Distribution::new_without_components(date, toolchain);
+    /// ```
     pub fn new_without_components(
         date: rust_toolchain::ReleaseDate,
         toolchain: rust_toolchain::Toolchain,
@@ -41,11 +73,46 @@ impl Distribution {
     }
 
     /// Returns a shared reference to the release date.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// # let date = ReleaseDate::new(2018, 12, 6);
+    /// # let toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    /// #
+    /// // Creating a Rust distribution for Rust 1.31 which was released on 2018-12-06
+    /// let distribution = Distribution::new_without_components(date, toolchain);
+    ///
+    /// assert_eq!(distribution.date(), &ReleaseDate::new(2018, 12, 6));
+    /// ```
     pub fn date(&self) -> &rust_toolchain::ReleaseDate {
         &self.date
     }
 
     /// Returns an exclusive reference to the release date.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// let date = ReleaseDate::new(2018, 12, 6);
+    /// let toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    /// #
+    /// // Creating a Rust distribution for Rust 1.31 which was released on 2018-12-06
+    /// let mut distribution = Distribution::new_without_components(date, toolchain);
+    ///
+    /// // Modifying the release date!
+    /// *distribution.date_mut() = ReleaseDate::new(2018, 1, 1);
+    ///
+    /// assert_eq!(distribution.date(), &ReleaseDate::new(2018, 1, 1));
+    /// ```
     ///
     /// # A note on modifying nightly releases
     ///
@@ -84,6 +151,26 @@ impl Distribution {
     }
 
     /// Returns a shared reference to the [`rust_toolchain::Toolchain`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// # let date = ReleaseDate::new(2018, 12, 6);
+    /// # let creation_toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    /// #
+    /// // Creating a Rust distribution for Rust 1.31 which was released on 2018-12-06
+    /// let distribution = Distribution::new_without_components(date, creation_toolchain);
+    ///
+    /// // Get the toolchain
+    /// let toolchain = distribution.toolchain();
+    ///
+    /// assert_eq!(toolchain.version().unwrap(), &RustVersion::new(1, 31, 0));
+    /// assert!(toolchain.channel().is_stable());
+    /// ```
     pub fn toolchain(&self) -> &rust_toolchain::Toolchain {
         &self.toolchain
     }
@@ -99,47 +186,157 @@ impl Distribution {
     ///
     /// # Example
     ///
-    /// ```rust
-    /// use rust_releases_core2::Distribution;
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// # let date = ReleaseDate::new(2018, 12, 6);
+    /// # let toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    /// #
+    /// let default_component = Component::new_component("a-default-component");
+    /// let optional_component = Component::new_extension("an-optional-component");
     ///
-    /// let release_date = rust_toolchain::ReleaseDate::new(2023, 1, 1);
-    /// let version = rust_toolchain::RustVersion::new(1, 31, 1);
+    /// // Creating a Rust distribution with a default and an optional component.
+    /// let distribution = Distribution::new(date, toolchain, [default_component, optional_component]);
     ///
-    /// let channel = rust_toolchain::Channel::beta(version);
-    /// let platform = rust_toolchain::Platform::host();
+    /// let component = distribution.find_component("an-optional-component").unwrap();
     ///
-    /// let toolchain = rust_toolchain::Toolchain::new(channel, platform);
+    /// assert_eq!(component.name, "an-optional-component");
+    /// assert!(component.optional);
     ///
-    /// let release = Distribution::new(release_date, toolchain, []);
-    /// let component = release.find_component("hello");
+    /// let not_included = distribution.find_component("not-this-one");
     ///
-    /// assert!(component.is_none());
+    /// assert!(not_included.is_none());
     /// ```
     pub fn find_component(&self, name: &str) -> Option<&rust_toolchain::Component> {
         self.components.iter().find(|f| f.name == name)
     }
 
     /// Returns an iterator over the components which are enabled by default.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// # let date = ReleaseDate::new(2018, 12, 6);
+    /// # let toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    /// #
+    /// let default_component = Component::new_component("a-default-component");
+    /// let optional_component = Component::new_extension("an-optional-component");
+    ///
+    /// // Creating a Rust distribution with a default and an optional component.
+    /// let distribution = Distribution::new(date, toolchain, [default_component, optional_component]);
+    ///
+    /// let subset_of_components = distribution.default_components().collect::<Vec<_>>();
+    ///
+    /// assert_eq!(subset_of_components.len(), 1);
+    /// assert_eq!(&subset_of_components[0].name, &"a-default-component");
+    /// assert!(!&subset_of_components[0].optional);
+    /// ```
     pub fn default_components(&self) -> impl Iterator<Item = &rust_toolchain::Component> {
         self.components.iter().filter(|f| !f.optional)
     }
 
     /// Returns an iterator over the components which are optional, and not installed by default.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// # let date = ReleaseDate::new(2018, 12, 6);
+    /// # let toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    /// #
+    /// let default_component = Component::new_component("a-default-component");
+    /// let optional_component = Component::new_extension("an-optional-component");
+    ///
+    /// // Creating a Rust distribution with a default and an optional component.
+    /// let distribution = Distribution::new(date, toolchain, [default_component, optional_component]);
+    ///
+    /// let subset_of_components = distribution.extension_components().collect::<Vec<_>>();
+    ///
+    /// assert_eq!(subset_of_components.len(), 1);
+    /// assert_eq!(&subset_of_components[0].name, &"an-optional-component");
+    /// assert!(&subset_of_components[0].optional)
+    /// ```
     pub fn extension_components(&self) -> impl Iterator<Item = &rust_toolchain::Component> {
         self.components.iter().filter(|f| f.optional)
     }
 
     /// Whether this is a _stable_ release.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// # let date = ReleaseDate::new(2018, 12, 6);
+    /// # let toolchain = Toolchain::new(Channel::stable(RustVersion::new(1, 31, 0)), Platform::host());
+    /// #
+    /// // Creating a Rust distribution for Rust 1.31 which was released on 2018-12-06
+    /// let distribution = Distribution::new_without_components(date, toolchain);
+    ///
+    /// assert!(distribution.is_stable());
+    ///
+    /// assert!(!distribution.is_beta());
+    /// assert!(!distribution.is_nightly());
+    /// ```
     pub fn is_stable(&self) -> bool {
         self.toolchain.channel.is_stable()
     }
 
     /// Whether this is a _beta_ release.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// # let date = ReleaseDate::new(2018, 12, 6);
+    /// # let toolchain = Toolchain::new(Channel::beta(RustVersion::new(1, 32, 0)), Platform::host());
+    /// #
+    /// // Creating a Rust distribution for Rust 1.32-beta which was released on 2018-12-06
+    /// let distribution = Distribution::new_without_components(date, toolchain);
+    ///
+    /// assert!(distribution.is_beta());
+    ///
+    /// assert!(!distribution.is_stable());
+    /// assert!(!distribution.is_nightly());
+    /// ```
     pub fn is_beta(&self) -> bool {
         self.toolchain.channel.is_beta()
     }
 
     /// Whether this is a _nightly_ release.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::iter;
+    /// # use rust_releases_core2::Distribution;
+    /// # use rust_toolchain::{Channel, Component, Platform, ReleaseDate, RustVersion, Toolchain};
+    /// #
+    /// # let date = ReleaseDate::new(2018, 12, 6);
+    /// # let toolchain = Toolchain::new(Channel::nightly(date.clone()), Platform::host());
+    /// #
+    /// // Creating a Rust distribution for the Rust nightly of 2018-12-06
+    /// let distribution = Distribution::new_without_components(date, toolchain);
+    ///
+    /// assert!(distribution.is_nightly());
+    ///
+    /// assert!(!distribution.is_stable());
+    /// assert!(!distribution.is_beta())
+    /// ```
     pub fn is_nightly(&self) -> bool {
         self.toolchain.channel.is_nightly()
     }

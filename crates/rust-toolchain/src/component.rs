a911a393722af08c4d02c598ac43b5ba6bff1ac5
Martijn <garm@ilumeo.com>
2025-02-21 17:42:49 +0100

    Put implementation detail of component name type behind method

--- crates/rust-toolchain/src/component.rs
+++ crates/rust-toolchain/src/component.rs
@@ -2,11 +2,26 @@ use std::borrow::Cow;
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
 pub struct Component {
-    pub name: Cow<'static, str>,
+    name: Cow<'static, str>,
 }
 
 impl Component {
     pub fn new(name: impl Into<Cow<'static, str>>) -> Self {
         Self { name: name.into() }
+    }
+
+    pub fn name(&self) -> &str {
+        self.name.as_ref()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_instance() {
+        let c = Component::new("sample");
+        assert_eq!(c.name(), "sample");
     }
 }
